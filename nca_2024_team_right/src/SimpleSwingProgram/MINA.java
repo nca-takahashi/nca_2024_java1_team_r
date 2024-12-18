f703da1e4c156366aa245ee41e713e2814fe9c50
motozawamina <ncr2419si0003@edu,nca,ac,jp>
2024-12-18 17:41:48 +0900

    まだ製作中…暫しお待ちを…

--- nca_2024_team_right/src/SimpleSwingProgram/MINA.java
+++ nca_2024_team_right/src/SimpleSwingProgram/MINA.java
@@ -8,7 +8,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class MINA extends JFrame {
+public class MINA extends JFrame { // Use lowercase class name
 
   public static void main(String[] args) { // Use String[] args for main method
     MINA frame = new MINA("MyTitle");
@@ -31,6 +31,97 @@ public class MINA extends JFrame {
       System.out.println("Error: Image not found!");
     }
 
-    getContentPane().add(panel); // Add panel to content pane
-  }
+
+
+// Load and display the image
+URL imageURL1 = getClass().getClassLoader().getResource("resources/B.png");
+if (imageURL1 != null) {
+  ImageIcon image = new ImageIcon(imageURL1);
+  JLabel imageLabel = new JLabel(image);
+  panel.add(imageLabel);
+} else {
+  System.out.println("Error: Image not found!");
 }
+
+//Load and display the image
+URL imageURL2 = getClass().getClassLoader().getResource("resources/C.png");
+if (imageURL2 != null) {
+ImageIcon image = new ImageIcon(imageURL2);
+JLabel imageLabel = new JLabel(image);
+panel.add(imageLabel);
+} else {
+System.out.println("Error: Image not found!");
+}
+
+//Load and display the image
+URL imageURL3 = getClass().getClassLoader().getResource("resources/D.png");
+if (imageURL3 != null) {
+ImageIcon image = new ImageIcon(imageURL3);
+JLabel imageLabel = new JLabel(image);
+panel.add(imageLabel);
+} else {
+System.out.println("Error: Image not found!");
+}
+
+//Load and display the image
+URL imageURL4 = getClass().getClassLoader().getResource("resources/b.png");
+if (imageURL4 != null) {
+ImageIcon image = new ImageIcon(imageURL4);
+JLabel imageLabel = new JLabel(image);
+panel.add(imageLabel);
+} else {
+System.out.println("Error: Image not found!");
+}
+
+//Load and display the image
+URL imageURL5 = getClass().getClassLoader().getResource("resources/b.png");
+if (imageURL5 != null) {
+ImageIcon image = new ImageIcon(imageURL5);
+JLabel imageLabel = new JLabel(image);
+panel.add(imageLabel);
+} else {
+System.out.println("Error: Image not found!");
+}
+
+//Load and display the image
+URL imageURL6 = getClass().getClassLoader().getResource("resources/b.png");
+if (imageURL6 != null) {
+ImageIcon image = new ImageIcon(imageURL6);
+JLabel imageLabel = new JLabel(image);
+panel.add(imageLabel);
+} else {
+System.out.println("Error: Image not found!");
+}
+
+
+//Load and display the image
+URL imageURL7 = getClass().getClassLoader().getResource("resources/b.png");
+if (imageURL7 != null) {
+ImageIcon image = new ImageIcon(imageURL7);
+JLabel imageLabel = new JLabel(image);
+panel.add(imageLabel);
+} else {
+System.out.println("Error: Image not found!");
+}
+
+//Load and display the image
+URL imageURL8 = getClass().getClassLoader().getResource("resources/b.png");
+if (imageURL8 != null) {
+ImageIcon image = new ImageIcon(imageURL8);
+JLabel imageLabel = new JLabel(image);
+panel.add(imageLabel);
+} else {
+System.out.println("Error: Image not found!");
+}
+
+//Load and display the image
+URL imageURL9 = getClass().getClassLoader().getResource("resources/b.png");
+if (imageURL9 != null) {
+ImageIcon image = new ImageIcon(imageURL9);
+JLabel imageLabel = new JLabel(image);
+panel.add(imageLabel);
+} else {
+System.out.println("Error: Image not found!");
+}}}
+
+
